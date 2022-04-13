58694f1d3e91bdf143ada387d796cd9092fbcb28
Miftah Heriyana <miftah09heriyana@gmail.com>
2022-04-13 13:25:35 +0700

    first commit

---
+++ ifelse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author raffi
+ */
+public class ifelse {
+    public static void main (String[] args){
+      int a = 20;
+      int b = 30;
+      
+      if(a<b){
+        System.out.print("a is smaller than b");
+      }else{
+        System.out.print("b is bigger than a");
+      }
+   }
+}
+    
+
---
+++ loop.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author raffi
+ */
+public class loop {
+    public static void main(String[] args){
+        int i;
+        for (i=1; i<=30; i++){
+           System.out.print("CCIT" +i);       
+        }
+    }
+}
